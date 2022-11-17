e1a65f28ad28e3c72be1723fe9ebcc0e0d6ec129
enjuichang <55577469+enjuichang@users.noreply.github.com>
2022-11-17 14:43:59 +0800

    debug tempfile

--- app.py
+++ app.py
@@ -5,23 +5,20 @@ import tempfile
 
 st.title("Audio Transcription and Translation with Whisper")
 
-audio_file = st.file_uploader("Choose a file", type= ["wav","mp3","m4a"])
-if audio_file is not None:
-    fp = tempfile.TemporaryFile()
-    fp.write(audio_file)
-    st.success("Saved File")
-
 model = whisper.load_model("base")
 
-if st.sidebar.button("Transcribe"):
-    if audio_file is not None:
-        st.sidebar.text("Transcribing...")
-        fp.seek(0)
-        print(fp)
-        print(fp.read())
-        transcript = model.transcribe(fp.read().name)
-        fp.close()
-        st.sidebar.success("Transcription complete")
-        st.text(transcript["text"])
-    else:
-        st.error("Please Upload an Audio File")
+audio_file = st.file_uploader("Choose a file", type= ["wav","mp3","m4a"])
+if audio_file is not None:
+    with tempfile.NamedTemporaryFile(delete=False) as temp:
+        temp.write(audio_file.getbuffer())
+        st.success("Saved File")
+
+        if st.sidebar.button("Transcribe"):
+            if audio_file is not None:
+                st.sidebar.text("Transcribing...")
+                transcript = model.transcribe(temp.name)
+                st.sidebar.success("Transcription complete")
+                st.text(transcript["text"])
+            else:
+                st.error("Please Upload an Audio File")
+
