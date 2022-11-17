de3398d957a8988a267007ebe57fc8cf6d129d37
enjuichang <55577469+enjuichang@users.noreply.github.com>
2022-11-17 14:51:38 +0800

    debug tempfile

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = whisper.load_model("base")
 
 audio_file = st.file_uploader("Choose a file", type= ["wav","mp3","m4a"])
 if audio_file is not None:
-    with tempfile.NamedTemporaryFile(delete=False) as temp:
+    with tempfile.NamedTemporaryFile(dir="./", delete=False) as temp:
         temp.write(audio_file.getbuffer())
         st.success("Saved File")
 
@@ -17,7 +17,6 @@ if audio_file is not None:
             if audio_file is not None:
                 st.sidebar.text("Transcribing...")
                 st.sidebar.text(temp.name)
-                st.sidebar.text(temp)
                 transcript = model.transcribe(temp.name)
                 st.sidebar.success("Transcription complete")
                 st.text(transcript["text"])
