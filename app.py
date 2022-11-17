8c1a38586da5c1a3b0cd5b7cc3442aed4b0c8064
enjuichang <55577469+enjuichang@users.noreply.github.com>
2022-11-17 14:31:43 +0800

    debug tempfile

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("Audio Transcription and Translation with Whisper")
 audio_file = st.file_uploader("Choose a file", type= ["wav","mp3","m4a"])
 if audio_file is not None:
     fp = tempfile.TemporaryFile()
-    fp.write(audio_file.getbuffer())
+    fp.write(audio_file)
     st.success("Saved File")
 
 model = whisper.load_model("base")
