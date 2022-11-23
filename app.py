0becc1376b8f1386a9de42b157ec7ae60946ad01
enjuichang <55577469+enjuichang@users.noreply.github.com>
2022-11-23 17:02:54 +0800

    Separate local and cloud apps

--- app.py
+++ app.py
@@ -1,33 +1,25 @@
 import streamlit as st
 import whisper
-import os
+import tempfile
 
 
 st.title("Audio Transcription and Translation with Whisper")
 
-audio_file = st.file_uploader("Choose a file", type= ["wav","mp3","m4a"])
-if audio_file is not None:
-    with open(os.path.join("tempDir",audio_file.name),"wb") as f: 
-        f.write(audio_file.getbuffer())
-    st.success("Saved File")
-
 model = whisper.load_model("base")
 
+audio_file = st.file_uploader("Choose a file", type= ["wav","mp3","m4a"])
+if audio_file is not None:
+    with tempfile.NamedTemporaryFile(delete=False) as temp:
+        temp.write(audio_file.getbuffer())
+        st.success("Saved File")
 
-if st.sidebar.button("Transcribe"):
-    if audio_file is not None:
-        st.sidebar.text("Transcribing...")
-        transcript = model.transcribe(os.path.join("tempDir",audio_file.name))
+        if st.sidebar.button("Transcribe"):
+            if audio_file is not None:
+                st.sidebar.text("Transcribing...")
+                st.sidebar.text(temp.name)
+                transcript = model.transcribe(temp.name)
+                st.sidebar.success("Transcription complete")
+                st.text(transcript["text"])
+            else:
+                st.error("Please Upload an Audio File")
 
-        st.sidebar.success("Transcription complete")
-        st.text(transcript["text"][:100])
-    else:
-        st.error("Please Upload an Audio File")
-
-if st.sidebar.button("Download"):
-    if transcript is not None:
-        st.sidebar.text("Downloading...")
-        st.sidebar.success("Download complete")
-        st.download_button("Download", transcript["text"])
-    else:
-        st.error("Please Transcribe an Audio File")
